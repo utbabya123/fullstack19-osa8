789d7c1dc918cdb76bc6874b2348661941d6fe76
unknown <jarkko.hulkko@japo.fi>
2019-12-08 06:56:42 +0200

    Tehtävä 8.14

---
+++ library-backend/index.js
@@ -0,0 +1,113 @@
+require('dotenv').config()
+const { ApolloServer, gql } = require('apollo-server')
+const mongoose = require('mongoose')
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.set('useFindAndModify', false)
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(yMONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+const typeDefs = gql`
+  type Book {
+    title: String!
+    author: Author!
+    published: Int!
+    genres: [String!]!
+    id: ID!
+  }
+
+  type Author {
+    name: String!,
+    born: Int,
+    bookCount: Int!
+    id: ID!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+`
+
+const resolvers = {
+  Query: {
+    bookCount: () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    allBooks: (root, args) => {
+      return Book.find({}).populate('author')
+      // const booksByAuthor = args.author ? books.filter(b => b.author === args.author) : books
+      // return args.genre ? booksByAuthor.filter(b => b.genres.includes(args.genre)) : booksByAuthor
+    },
+    allAuthors: () => {
+      return Author.find({})
+    }
+  },
+  Author: {
+    bookCount: (root) => {
+      const booksByAuthor = Book.find({ author: root.id })
+      return booksByAuthor.count()
+    }
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      try {
+        let author = await Author.findOne({ name: args.author})
+        if (!author) {
+          author = new Author({ name: args.author })
+          await author.save()
+        }
+        const book = new Book({ ...args, author: author._id })
+        await book.save()
+
+        return book
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    editAuthor: async (root, args) => {
+      try {
+        const author = await Author.findOne({ name: args.name })
+        author.born = args.setBornTo
+        await author.save()
+
+        return author
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
